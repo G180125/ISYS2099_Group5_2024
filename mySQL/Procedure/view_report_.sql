dd7b0c3714df21451dbff11224e1bff3300b6546
huan27042003 <huan27042003@gmail.com>
2024-08-28 17:51:33 +0700

    Add view_report sql

---
+++ mySQL/Procedure/view_report_.sql
@@ -0,0 +1,38 @@
+CREATE VIEW all_patient_treatment_history AS
+SELECT 
+    history_id,
+    treatment_name,
+    treatment_date,
+    appointment_id,
+    status
+FROM 
+    treatment_record;
+
+-- DELIMITER $$
+-- View a patient treatment for a given duration
+CREATE PROCEDURE view_patient_treatment_for_given_duration(
+    IN p_date DATE
+)
+BEGIN
+    SELECT * 
+    FROM treatment_record
+    WHERE treatment_date = p_date;
+END ;
+
+-- DELIMITER $$
+-- View all patient treatment in a given duration
+CREATE PROCEDURE view_patient_treatment_in_given_duration(
+    IN p_start_date DATE,
+    IN p_end_date DATE
+)
+BEGIN
+    SELECT * 
+    FROM treatment_record
+    WHERE treatment_date BETWEEN p_start_date AND p_end_date;
+END ;
+
+
+
+-- View job change history of a staff (need relation)
+-- View the work of a doctor in a given duration (need relation)
+-- View the work of all doctors in a given duration (need relation)
