0fa7dcf4a096c60ac497fedb6de753ea0a75578b
huan27042003 <huan27042003@gmail.com>
2024-08-31 13:15:24 +0700

    refactor code

--- app/controllers/ticketController.js
+++ app/controllers/ticketController.js
@@ -3,23 +3,25 @@ const cookieParser = require("cookie-parser");
 const mysqlClient = require("../databases/mysqlClient");
 const httpStatus = require("../utils/httpStatus.js");
 const mysqlService = require("../services/mysqlService");
+const { introspect } = require("../utils");
 const app = express();
 app.use(cookieParser());
 
 const ticketController = {
     createTicket: async (req, res) => {
         try {
-            // const { email } = req.cookies;
-            const { staffId, newSalary, newJobType, newDepartmentID } = req.body;
-
+            const { accessToken } = req.cookies;
+            const payload = introspect(accessToken, process.env.ACCESS_TOKEN_SECRET);
             // Check if the staff ID is valid
-            // const staffIdFromDb = await mysqlService.getStaffId(email);
-            // if (!staffIdFromDb) {
-            //     return res
-            //         .status(httpStatus.BAD_REQUEST().code)
-            //         .json({ error: httpStatus.BAD_REQUEST("No Staff member found to make the request").message });
-            // }
+            const staffEmail = payload.email;
+            const staffId = await mysqlService.getStaffId(staffEmail);
+            if (!staffId) {
+                return res
+                    .status(httpStatus.UNAUTHORIZED().code)
+                    .json({ error: httpStatus.UNAUTHORIZED("Invalid staff ID").message });
+            }
 
+            const {newSalary, newJobType, newDepartmentID } = req.body;
             // Set default values to NULL if they are not provided
             const salary = newSalary !== undefined && newSalary !== '' ? newSalary : null;
             const jobType = newJobType !== undefined && newJobType !== '' ? newJobType : null;
@@ -29,25 +31,23 @@ const ticketController = {
                 CALL create_ticket_for_update(?, ?, ?, ?, @result, @message);
             `;
 
-            const [resultSet] = await mysqlClient.poolStaff.query(query, [
+            const [rows] = await mysqlClient.poolStaff.query(query, [
                 staffId, salary, jobType, departmentID
             ]);
 
-            // Check for rollback message in the result
-            const result = resultSet[1]?.[0];
-            
-            const errorMessage = result?.message;
-            const operationResult = result?.result;
+            const message = rows[0][0].message;
+            console.log(rows);
 
-            if (operationResult === 0) {
+            if (result == 0) {
                 return res
-                    .status(httpStatus.BAD_REQUEST().code)
-                    .json({ error: httpStatus.BAD_REQUEST(errorMessage).message });
-            }
+                  .status(httpStatus.BAD_REQUEST().code)
+                  .json({ error: httpStatus.BAD_REQUEST(message).message });
+              }
 
             return res
                 .status(httpStatus.OK().code)
-                .json({ message: httpStatus.OK(`Ticket created Successfully`).message });
+                .json({ message: message });
+            
 
         } catch (error) {
             console.error('Error in createTicket:', error);
